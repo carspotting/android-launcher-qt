d10145720f72b62f59ef1b296f03a1b0b1ecb809
carspotting <86288177+carspotting@users.noreply.github.com>
2021-06-22 17:57:37 +0800

    Update Volla_cn.ts

--- translations/Volla_cn.ts
+++ translations/Volla_cn.ts
@@ -5,42 +5,42 @@
     <name>AppGrid</name>
     <message>
         <source>Apps</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">应用</translation>
     </message>
     <message>
         <source>Filter apps</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">筛选应用</translation>
     </message>
 </context>
 <context>
     <name>Browser</name>
     <message>
         <source>Show Dialer</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">电话</translation>
     </message>
     <message>
         <source>Open Camera</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">照相</translation>
     </message>
     <message>
         <source>Agenda</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">日程</translation>
     </message>
     <message>
         <source>Gallery</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">图库</translation>
     </message>
     <message>
         <source>Recent News</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">最近新闻</translation>
     </message>
     <message>
         <source>Recent Threads</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">最近进程</translation>
     </message>
     <message>
-        <source>Revent People</source>
-        <translation type="unfinished"></translation>
+        <source>Recent People</source>
+        <translation type="unfinished">近期联系人</translation>
     </message>
 </context>
 <context>
@@ -54,43 +54,43 @@
     <name>Collections</name>
     <message>
         <source>People</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">联系人</translation>
     </message>
     <message>
         <source>Find people ...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">寻找联系人</translation>
     </message>
     <message>
         <source>Threads</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">进程</translation>
     </message>
     <message>
         <source>Find thread ...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">搜索进程</translation>
     </message>
     <message>
         <source>News</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">新闻</translation>
     </message>
     <message>
         <source>Find news ...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">搜索新闻</translation>
     </message>
     <message>
         <source>Filter collections</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">筛选集合</translation>
     </message>
     <message>
         <source>Call</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">拨打电话</translation>
     </message>
     <message>
         <source>Send Message</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">发送短信</translation>
     </message>
     <message>
         <source>Send Email</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">发送邮件</translation>
     </message>
     <message>
         <source>Private</source>
@@ -98,225 +98,225 @@
     </message>
     <message>
         <source>New message</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">新消息</translation>
     </message>
     <message>
         <source>New calls</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">新来电</translation>
     </message>
     <message>
         <source>New call</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">新来电</translation>
     </message>
     <message>
         <source>You</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">本机</translation>
     </message>
     <message>
         <source>Could not load RSS feed: </source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">无法加载RSS节点</translation>
     </message>
     <message>
         <source>Invalid RSS feed: </source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">无效RSS节点</translation>
     </message>
     <message>
         <source>Missing RSS item: </source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">缺失RSS项</translation>
     </message>
 </context>
 <context>
     <name>Conversation</name>
     <message>
         <source>Conversation</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">会话</translation>
     </message>
     <message>
         <source>Filter messages ...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">筛选信息</translation>
     </message>
     <message>
         <source>Not yet supported</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">暂不支持</translation>
     </message>
 </context>
 <context>
     <name>Details</name>
     <message>
         <source>Not yet supported</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">暂不支持</translation>
     </message>
     <message>
         <source>Open in browser</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">在浏览器中打开</translation>
     </message>
     <message>
         <source>Share</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">分享</translation>
     </message>
 </context>
 <context>
     <name>Feed</name>
     <message>
         <source>Feed</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">信息源</translation>
     </message>
     <message>
         <source>Filter news ...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">筛选信息</translation>
     </message>
     <message>
         <source>Not yet supported</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">暂不支持</translation>
     </message>
 </context>
 <context>
     <name>Settings</name>
     <message>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">设置</translation>
     </message>
     <message>
         <source>Dark Mode</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">深色模式</translation>
     </message>
     <message>
         <source>Light Mode</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">浅色模式</translation>
     </message>
     <message>
         <source>Translucent Mode</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">半透明模式</translation>
     </message>
     <message>
         <source>News Channels</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">新闻频道</translation>
     </message>
     <message>
         <source>Shortcuts</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">快捷方式</translation>
     </message>
     <message>
         <source>Experimental</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">测试</translation>
     </message>
     <message>
         <source>Fullscreen</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">全屏</translation>
     </message>
     <message>
         <source>Use colored app icons</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">使用彩色应用图标</translation>
     </message>
     <message>
         <source>Show apps at startup</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">在开始页面显示应用程序</translation>
     </message>
 </context>
 <context>
     <name>Springboard</name>
     <message>
         <source>Springboard</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">开始</translation>
     </message>
     <message>
         <source>Type anything</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">输入任何信息</translation>
     </message>
     <message>
         <source>Sorry. I couldn&apos;t find a phone number for this contact</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">抱歉，未查询到该联系人电话号码</translation>
     </message>
     <message>
         <source>Sorry. I couldn&apos;t identify the contact</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">抱歉，未能识别该联系人</translation>
     </message>
     <message>
         <source>Sorry. Contact has no email address</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">抱歉，未查询到该联系人邮件地址信息</translation>
     </message>
     <message>
         <source>My event</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">我的事项</translation>
     </message>
     <message>
         <source>Show Dialer</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">拨号</translation>
     </message>
     <message>
         <source>Open Camera</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">打开相机</translation>
     </message>
     <message>
         <source>Show Agenda</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">显示日程</translation>
     </message>
     <message>
         <source>Create Event</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">创建新事项</translation>
     </message>
     <message>
         <source>Show Notes</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">显示记事本</translation>
     </message>
     <message>
         <source>Gallery</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">图库</translation>
     </message>
     <message>
         <source>Recent News</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">最近新闻</translation>
     </message>
     <message>
         <source>Recent Threads</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">最近进程</translation>
     </message>
     <message>
         <source>Recent People</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">近期联系人</translation>
     </message>
     <message>
         <source>Sorry, no contact was selected</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">抱歉，该联系人未知</translation>
     </message>
     <message>
         <source>Sorry, the mobile phone number is unknown</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">抱歉，该电话号码未知</translation>
     </message>
     <message>
         <source>Your note was successfully stored</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">您的笔记已保存</translation>
     </message>
 </context>
 <context>
     <name>main</name>
     <message>
         <source>Volla</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Volla</translation>
     </message>
     <message>
         <source>Send message</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">发送信息</translation>
     </message>
     <message>
         <source>Send email</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">发送邮件</translation>
     </message>
     <message>
         <source>Create note</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">创建记事本</translation>
     </message>
     <message>
         <source>Search web</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">搜索网页</translation>
     </message>
     <message>
         <source>Call</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">拨打</translation>
     </message>
     <message>
         <source>Open in browser</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">在浏览器中打开</translation>
     </message>
     <message>
         <source>Add feed to collection</source>
@@ -324,71 +324,71 @@
     </message>
     <message>
         <source>Missing message text</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">缺失信息内容</translation>
     </message>
     <message>
         <source>Message sent</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">信息已发送</translation>
     </message>
     <message>
         <source>Generic failure</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">一般故障</translation>
     </message>
     <message>
         <source>No service</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">无服务</translation>
     </message>
     <message>
         <source>Null PDU</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">空白协议数据单元</translation>
     </message>
     <message>
         <source>Radio off</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">关闭无线电</translation>
     </message>
     <message>
         <source>Message delivered</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">信息已发送</translation>
     </message>
     <message>
         <source>Message not delivered</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">信息未发送</translation>
     </message>
     <message>
         <source>Show Dialer</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">拨号</translation>
     </message>
     <message>
         <source>Camera</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">照相</translation>
     </message>
     <message>
         <source>Agenda</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">日程</translation>
     </message>
     <message>
         <source>Gallery</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">图库</translation>
     </message>
     <message>
         <source>Notes</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">记事本</translation>
     </message>
     <message>
         <source>Create Event</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">创建新事项</translation>
     </message>
     <message>
         <source>Recent People</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">近期联络人</translation>
     </message>
     <message>
         <source>Recent Threads</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">近期进程</translation>
     </message>
     <message>
         <source>Recent News</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">近期新闻</translation>
     </message>
     <message>
         <source>Just now</source>
@@ -396,59 +396,59 @@
     </message>
     <message>
         <source>minutes ago</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">数分钟前</translation>
     </message>
     <message>
         <source>Today</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">今日</translation>
     </message>
     <message>
         <source>Yesterday</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">昨日</translation>
     </message>
     <message>
         <source>You have alresdy subscribed the feed</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">您已订阅该信息源</translation>
     </message>
     <message>
         <source>Invalid news ID</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">无效新闻ID</translation>
     </message>
     <message>
         <source>Invalid RSS feed: </source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">无效RSS信息源</translation>
     </message>
     <message>
         <source>Not yet supported</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">暂未支持</translation>
     </message>
     <message>
         <source>Send home email</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">发送家庭邮件</translation>
     </message>
     <message>
         <source>Send work email</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">发送工作邮件</translation>
     </message>
     <message>
         <source>Send other email</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">发送其他邮件</translation>
     </message>
     <message>
         <source>Call on cell phone</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">使用移动电话呼叫</translation>
     </message>
     <message>
         <source>Call at home</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">在家呼叫</translation>
     </message>
     <message>
         <source>Call at work</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">工作呼叫</translation>
     </message>
     <message>
         <source>Call other phone</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">拨打其他电话</translation>
     </message>
 </context>
 </TS>
